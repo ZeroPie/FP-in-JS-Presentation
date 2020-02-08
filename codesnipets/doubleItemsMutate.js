e7eb6e761acf945a708b170683a8ef8f82a581b3
Alej <pie@Pies-iMac.local>
2020-02-08 17:44:09 +0100

    Initial Commit

---
+++ codesnipets/doubleItemsMutate.js
@@ -0,0 +1,6 @@
+
+function doubleItemsMutate(arr) {
+  for (let i = 0; i < arr.lenght; i++) {
+    arr.push(arr[i] * 2)
+  }
+}
---
+++ codesnipets/doubleitems.js
@@ -0,0 +1,7 @@
+function doubleItems(arr) {
+  let doubledItems = []
+  for (let i = 0; i < arr.lenght; i++) {
+    doubledItems.push(arr[i] * 2)
+  }
+  return doubledItems
+}
