78442683d517c1c9c26d4224ff1c96e507c96c58
Alej <pie@Pies-iMac.local>
2020-02-09 20:30:36 +0100

    Add More Stuff

---
+++ codesnipets/daysThisMonthPure.js
@@ -0,0 +1,5 @@
+const daysThisMonth = (y, m) => {
+  let start = new Date(y, m - 1, 1)
+    , end = new Date(y, m + 1, 1);
+  return (end - start) / (1000 * 60 * 60 * 24)
+}
---
+++ codesnipets/seagulsFlockFp.js
@@ -0,0 +1,8 @@
+const conjoin = (flockX, flockY) => flockX + flockY;
+const breed = (flockX, flockY) => flockX * flockY;
+
+const flockA = 4;
+const flockB = 2;
+const flockC = 0;
+const result =
+  conjoin(breed(flockB, conjoin(flockA, flockC)), breed(flockA, flockB));
