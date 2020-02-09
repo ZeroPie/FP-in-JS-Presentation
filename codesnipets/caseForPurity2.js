5508a66dca4cf3292245bb60ed2553d66f78d2b8
Alej <pie@Pies-iMac.local>
2020-02-09 12:59:52 +0100

    Add Pureness

---
+++ codesnipets/caseForPurity2.js
@@ -0,0 +1,9 @@
+const devs = [
+  { name: 'Joel', role: 'Dev' },
+  { name: 'Pascal', role: 'Dev' },
+  { name: 'Dariutsch', role: 'Dev' },
+]
+
+const teamCaptains = toTeamCaptains(devs)
+
+devs[0]["role"] // = ?
