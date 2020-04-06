f5c5607cd19d9c95d85191c889ff05c0c835ec70
Alejandro Palacios <alejandro.palacios@meinestadt.de>
2020-04-06 14:38:25 +0200

    Clean up

--- codesnipets/addCommentPure.js
+++ codesnipets/addCommentPure.js
@@ -7,5 +7,6 @@ const addComment = (uid, authorId, text) => {
   return createCommentElement(comment)
 }
 
+const uid = genUniqueId()
 addComment(uid, 130918, "Millionen deutschen werden reich mit diesem Trick")
 commentsList.appendChild(ele)
